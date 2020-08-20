60a1cce6e77f35c0d33f26960f7d940123d000b3
mdaniel543 <53839987+mdaniel543@users.noreply.github.com>
2020-08-19 21:15:57 -0600

    Interfaz

--- main.py
+++ main.py
@@ -1,78 +1,114 @@
-from os import system
-from io import open
-
-Encabezado = """USAC
-Facultad de Ingenieria
-Escuela de Ciencias y sistemas
-Organizacion de Lenguajes y Compiladores 1\n
-\t\t PROYECTO 1 \n
-"""
-print(Encabezado)
+from tkinter import Tk, Menu, messagebox, filedialog, ttk, Label, scrolledtext, INSERT, END, Button, Scrollbar, RIGHT, Y, Frame, Canvas, HORIZONTAL, VERTICAL, simpledialog
+from tkinter import *
+from tkinter import ttk
+from tkinter.filedialog import askopenfilename
+from tkinter import messagebox
 
 
-Menu = """---------------------------------------------
+class T:
+    archivo = ""
+    def analizar(self):
+        texto = self.editor.get("1.0",END)
+        print("analizando: "+texto)
 
-\t MENU: 
-1. Nuevo 
-2. Abrir
-3. Guardar
-4. Guardar como
-5. Ejecutar analisis
-6. Salir
-"""
+        self.printSalida()
+    #END
+    def printSalida(self):
+        texto = "Finalizo el analisis"
+        self.consola.insert(INSERT,texto)
 
+        messagebox.showerror("Error", "Texto a mostrar:\n")
+    #END
+    def nuevo(self):
+        global archivo
+        self.editor.delete(1.0, END)#ELIMINAR EL CONTENIDO
+        archivo = ""
 
-def abrirarchivo():
-    Abrir = """ 
-    *****************************************
+    def abrir(self):
+        global archivo
+        archivo = filedialog.askopenfilename(title = "Abrir Archivo")
 
-    Que tipo de archivo desea abrir:
-    1) .js
-    2) .css
-    3) .html
-    4) .rmt
-    """
-    print(Abrir)
-    a = int(input("Ingrese numero del archivo que desea abrir: "))
-    print("\n **************************************** \n")
-    print("\t SU ARCHIVO CONTIENE: \n\n")
-    if a == 1:
-        archivoJS = open("comentario.js", "r")
-        texto = archivoJS.read()
-        archivoJS.close()
-        print(texto)
-    else:
-        print("Incorrecto")
+        entrada = open(archivo)
+        content = entrada.read()
 
+        self.editor.delete(1.0, END)
+        self.editor.insert(INSERT, content)
+        entrada.close()
 
+    def salir(self):
+        value = messagebox.askokcancel("Salir", "Está seguro que desea salir?")
+        if value :
+            root.destroy()
 
+    def guardarArchivo(self):
+        global archivo
+        if archivo == "":
+            guardarComo()
+        else:
+            guardarc = open(archivo, "w")
+            guardarc.write(self.editor.get(1.0, END))
+            guardarc.close()
 
-val = True;
-while val == True:
-    print(Menu)
-    ingreso = int(input("Ingrese numero de la accion que desea realizar: "))
-    
-    if ingreso == 1:
-        print(1)
-    elif ingreso == 2:
-        abrirarchivo()
-    elif ingreso == 3:
-        print(3)
-    elif ingreso == 4:
-        print(4)
-    elif ingreso == 5:
-        print(5)
-    elif ingreso == 6:
-        print(6)
-        val = False;
-    else:
-        print("Numero de accion invalido! ")
+    def guardarComo(self):
+        global archivo
+        guardar = filedialog.asksaveasfilename(title = "Guardar Archivo", initialdir = "C:/")
+        fguardar = open(guardar, "w+")
+        fguardar.write(self.editor.get(1.0, END))
+        fguardar.close()
+        archivo = guardar
+    #END
 
+    def __init__(self, root):
+        root.title("Proyecto 1") 
+        root.configure(background = "lightgray")
+        self.barraMenu = Menu(root)
+        root.config(menu = self.barraMenu, width = 1375, height = 600)
 
+        self.archivoMenu = Menu(self.barraMenu, tearoff=0)
+        self.archivoMenu.add_command(label = "Nuevo", command = self.nuevo)
+        self.archivoMenu.add_command(label = "Abrir", command = self.abrir)
+        self.archivoMenu.add_command(label = "Guardar", command = self.guardarArchivo)
+        self.archivoMenu.add_command(label = "Guardar Como...", command = self.guardarComo)
+        self.archivoMenu.add_separator()
+        self.archivoMenu.add_command(label = "Salir", command = self.salir)
 
+        self.barraMenu.add_cascade(label = "Archivo", menu = self.archivoMenu)
+        self.barraMenu.add_command(label = "Ejecutar Analisis",  command = self.analizar)
+        self.barraMenu.add_command(label = "Salir",  command = self.salir)
 
+        frame = Frame(root, bg="lightgray")
+        canvas = Canvas(frame, bg="lightgray")
+        scrollbar = Scrollbar(frame, orient=VERTICAL, command=canvas.yview)
+        scroll = Frame(canvas, bg="lightgray")
 
-    
+        scroll.bind("<Configure>",lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
 
+        canvas.create_window((0, 0), window=scroll, anchor="nw")
 
+        canvas.configure(yscrollcommand=scrollbar.set, width = 1375, height = 600)
+
+        Label(scroll,text='ML WEB EDITOR', font = ("Arial", 16), background='lightgray', foreground = "black").grid(row=0,column=1)
+
+        Label(scroll,text='Lectura de archivo', font = ("Arial", 14), background='lightgray', foreground = "black").grid(row=1,column=0)
+        Label(scroll,text='Consola', font = ("Arial", 14), background='lightgray', foreground = "black").grid(row=1,column=2)
+
+        self.editor = scrolledtext.ScrolledText(scroll, undo = True, width = 80, height = 30, font = ("Arial", 10), background = 'lightblue',  foreground = "black")
+
+        self.editor.grid(row = 2, column = 0)
+
+        self.consola = scrolledtext.ScrolledText(scroll, undo = True, width = 80, height = 30, font = ("Arial", 10), background = 'black',  foreground = "white")
+
+        self.consola.grid(row = 2, column = 2)
+
+        frame.grid(sticky='news')
+        canvas.grid(row=0,column=1)
+        scrollbar.grid(row=0, column=2, sticky='ns')
+        self.editor.focus()
+        self.consola.focus()
+
+###################################################################################################
+if __name__ == '__main__':
+    root = Tk()
+    app = T(root)
+    root.mainloop()
 
